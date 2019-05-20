c2126ec77f5a73d7879d017d6493fb90ac32acbd
Milan Stute <mstute@seemoo.tu-darmstadt.de>
2019-05-20 16:50:49 +0200

    Add license header to .h and .c files

--- src/rx.h
+++ src/rx.h
@@ -1,3 +1,22 @@
+/*
+ * OWL: an open Apple Wireless Direct Link (AWDL) implementation
+ * Copyright (C) 2018  The Open Wireless Link Project (https://owlink.org)
+ * Copyright (C) 2018  Milan Stute
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef AWDL_RX_H_
 #define AWDL_RX_H_
 
