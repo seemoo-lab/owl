170c076bdf69922ba0d0f1f7515fcbf5457cdf9e
Milan Stute <mstute@seemoo.tu-darmstadt.de>
2019-12-17 23:58:12 +0100

    Update election tests

--- tests/test_awdl_election.cpp
+++ tests/test_awdl_election.cpp
@@ -32,6 +32,13 @@ TEST_ADDR(1);
 #define ASSERT_ETHEREQ(val1, val2) ASSERT_FALSE(compare_ether_addr(&val1, &val2))
 #define ASSERT_ETHERNEQ(val1, val2) ASSERT_TRUE(compare_ether_addr(&val1, &val2))
 
+void test_add_valid_peer(awdl_peers_t peers, const struct ether_addr *addr) {
+    struct awdl_peer *peer;
+    awdl_peer_add(peers, addr, 0, NULL, NULL);
+    awdl_peer_get(peers, addr, &peer);
+    peer->is_valid = true;
+}
+
 TEST(awdl_election, init) {
 	struct awdl_election_state s;
 	awdl_election_state_init(&s, &TEST_ADDR0);
@@ -54,11 +61,24 @@ TEST(awdl_election, elect_simple) {
 	awdl_election_state_init(&s, &TEST_ADDR0);
 	awdl_peer_state_init(&p);
 
-	awdl_peer_add(p.peers, &TEST_ADDR1, 0, NULL, NULL);
+    test_add_valid_peer(p.peers, &TEST_ADDR1);
 
 	awdl_election_run(&s, &p);
 
 	ASSERT_ETHEREQ(s.master_addr, TEST_ADDR1);
+}
+
+TEST(awdl_election, elect_simple_invalid) {
+    struct awdl_election_state s;
+    struct awdl_peer_state p;
+    awdl_election_state_init(&s, &TEST_ADDR0);
+    awdl_peer_state_init(&p);
+
+    awdl_peer_add(p.peers, &TEST_ADDR1, 0, NULL, NULL);
+
+    awdl_election_run(&s, &p);
+
+    ASSERT_ETHEREQ(s.master_addr, TEST_ADDR0);
 }
 
 TEST(awdl_election, counter_metric) {
@@ -68,7 +88,7 @@ TEST(awdl_election, counter_metric) {
 	awdl_election_state_init(&s, &TEST_ADDR0);
 	awdl_peer_state_init(&p);
 
-	awdl_peer_add(p.peers, &TEST_ADDR1, 0, NULL, NULL);
+    test_add_valid_peer(p.peers, &TEST_ADDR1);
 	awdl_peer_get(p.peers, &TEST_ADDR1, &peer);
 
 	awdl_election_run(&s, &p);
@@ -103,7 +123,7 @@ TEST(awdl_election, elect_cycle) {
 	awdl_election_state_init(&s, &TEST_ADDR0);
 	awdl_peer_state_init(&p);
 
-	awdl_peer_add(p.peers, &TEST_ADDR1, 0, NULL, NULL);
+    test_add_valid_peer(p.peers, &TEST_ADDR1);
 	awdl_peer_get(p.peers, &TEST_ADDR1, &peer);
 
 	peer->election.master_metric = 1000;
